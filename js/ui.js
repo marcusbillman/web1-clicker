e0ad292746cf86561d9cfa82378b38112e3b8e3a
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2020-04-17 14:55:09 +0200

    Remove ripple after animation

--- js/ui.js
+++ js/ui.js
@@ -5,4 +5,7 @@ rippleTemplate.classList.add("ripple");
 function ripple() {
     const newRipple = rippleTemplate.cloneNode();
     plusButtonWrapper.appendChild(newRipple);
+    newRipple.addEventListener("animationend", () => {
+        newRipple.remove();
+    });
 }
